838386bc973217cb6f2a78d7165eb5f32756ef97
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-01-04 13:57:19 +0200

    Format, add test image.

--- SeatingMap-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
+++ SeatingMap-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
@@ -8,7 +8,6 @@ import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import org.percepta.mgrankvi.FloorMap;
 import org.percepta.mgrankvi.Room;
 import org.percepta.mgrankvi.SeatingMap;
 import org.percepta.mgrankvi.Table;
@@ -24,39 +23,40 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class DemoUI extends UI {
 
-  @WebServlet(value = "/*", asyncSupported = true)
-  @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
-  public static class Servlet extends VaadinServlet {
-  }
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
 
-  @Override
-  protected void init(VaadinRequest request) {
+    @Override
+    protected void init(VaadinRequest request) {
 
-    // Initialize our new UI component
-    final SeatingMap component = new SeatingMap();
-    component.setSizeFull();
+        // Initialize our new UI component
+        final SeatingMap component = new SeatingMap();
+        component.setSizeFull();
 
-    ImageToLines imageToLines = new ImageToLines();
-    List<Line> thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorMarketing.png",new Point(150,150));
-    component.addRoom(1, thirdLines);
-    thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales.png",new Point(150,150));
-    Room room = component.addRoom(1, thirdLines);
-    Table table = new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t1.png", new Point(150, 150)));
-    table.setName("Terhi Testi");
-    room.addComponent(table);
-    room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t2.png",new Point(150,150))));
-    room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t3.png",new Point(150,150))));
-    room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t4.png",new Point(150,150))));
+        ImageToLines imageToLines = new ImageToLines();
+        List<Line> thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorMarketing.png", new Point(150, 150));
+        component.addRoom(1, thirdLines);
+        thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales.png", new Point(150, 150));
+        Room room = component.addRoom(1, thirdLines);
+        Table table = new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t1.png", new Point(150, 150)));
+        table.setName("Terhi Testi");
+        table.setImageUrl("https://vaadin.com/vaadin-theme/images/vaadin/vaadin-logo-small.png");
+        room.addComponent(table);
+        room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t2.png", new Point(150, 150))));
+        room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t3.png", new Point(150, 150))));
+        room.addComponent(new Table(imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorSales-t4.png", new Point(150, 150))));
 
-    thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorUtilities.png",new Point(150,150));
-    component.addLines(1, thirdLines);
+        thirdLines = imageToLines.getLines("/org/percepta/mgrankvi/demo/ThirdFloorUtilities.png", new Point(150, 150));
+        component.addLines(1, thirdLines);
 
-    // Show it in the middle of the screen
-    final VerticalLayout layout = new VerticalLayout();
-    layout.setStyleName("demoContentLayout");
-    layout.setSizeFull();
-    layout.addComponent(component);
-    layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
-    setContent(layout);
-  }
+        // Show it in the middle of the screen
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        layout.addComponent(component);
+        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+        setContent(layout);
+    }
 }
